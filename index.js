19a0b36ebe052f6afefb8a72b14363165a3d5b93
Vasanth Selvam <therealvasanth@gmail.com>
2017-05-23 16:53:57 +0530

    creating function for converting time into hex color code

---
+++ index.js
@@ -0,0 +1,16 @@
+var hexColor;
+var d = new Date();
+var h = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+
+var timeToHex = function(x,y,z){
+	x = x.toString();
+	y = y.toString();
+	z = z.toString();
+	hexColor = "#"+x+y+z;
+	console.log("Current time to color is : "+hexColor);
+}
+
+
+timeToHex(h,m,s);
